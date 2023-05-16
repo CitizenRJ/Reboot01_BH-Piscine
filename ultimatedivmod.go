267a7ce4b6a38d3a0fa7757c775dd440557ffce9
rjamal <rami@piercer.gg>
2023-05-16 15:45:08 +0300

    my very first go function

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,13 +1,6 @@
 package piscine
 
-import "fmt"
-
 func UltimateDivMod(a *int, b *int) {
-	if *b == 0 {
-		fmt.Println("Cannot divide by zero")
-		return
-	}
-
-	*a /= *b
-	*b %= *a
+	*a = *a / *b
+	*b = *a % *b
 }
