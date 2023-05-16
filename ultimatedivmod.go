f13645c75f5e8db5900e6686ae00de3336e54f96
rjamal <rami@piercer.gg>
2023-05-16 16:41:49 +0300

    my very first go function

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,18 +1,7 @@
 package piscine
 
 func UltimateDivMod(a *int, b *int) {
-	if *b == 0 {
-		return
-	}
-
-	if *a < 0 {
-		*a = -*a
-	}
-
-	if (*a % *b) == 0 {
-		*a /= *b
-		*b = *a % *b
-	} else {
-		return
-	}
+	t := *a
+	*a = *a / *b
+	*b = t % *b
 }
