ff008b75d8821b9c77731acbd86bb3f0e039c07e
rjamal <rami@piercer.gg>
2023-05-22 03:06:34 +0300

    Quest4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,19 +1,15 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-
-	if nb < 0 || nb > 12 {
+	if nb < 0 {
 		return 0
-	} else if nb == 0 {
-		return 1
 	}
-
-	c := nb
-	b := 1
-	for nb := 1; nb <= c; nb++ {
-
-		b = b * nb
+	result := 1
+	for i := 1; i <= nb; i++ {
+		result *= i
+		if result < 0 {
+			return 0 // overflow
+		}
 	}
-	return b
-
+	return result
 }
