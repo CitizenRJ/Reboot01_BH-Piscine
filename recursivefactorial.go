240201433291092fc1adc86cc53fe10121e7ba2b
rjamal <rami@piercer.gg>
2023-05-22 05:59:04 +0300

    Quest4

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,7 +5,7 @@ func RecursiveFactorial(nb int) int {
 		return 0
 	} else if nb == 0 {
 		return 1
-	} else if nb > 12 {
+	} else if nb > 20 {
 		return 0
 	} else {
 		return nb * RecursiveFactorial(nb-1)
