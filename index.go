93e83d3a1feb2cb847278741afcbe866b330a7ac
rjamal <rami@piercer.gg>
2023-05-22 23:39:32 +0300

    Quest5

--- index.go
+++ index.go
@@ -1,18 +1,16 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	for i := 0; i < len(s); i++ {
-		if s[i] == (toFind)[0] {
-			for j := 0; j < len(toFind); j++ {
-				if s[i+j] != toFind[j] {
-					break
-				}
-			}
-			if j == len(toFind) {
-				return 1
+	x := []rune(s)
+	y := []rune(toFind)
+	index := -1
+	for i := 0; i < len(x); i++ {
+		if len(y)+i <= len(x) {
+			if s[i:i+len(y)] == toFind {
+				index = i
+				break
 			}
 		}
 	}
-
-	return -1
+	return index
 }
