4633dbdac09416f17ababc72268b13c5ac10e4cd
rjamal <rami@piercer.gg>
2023-05-22 04:45:28 +0300

    Quest4

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,15 +1,20 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 || nb >= 13 {
-		return 0
-	}
+	result := 0
 
 	if nb == 0 {
 		return 1
 	}
 
-	result := nb * RecursiveFactorial(nb-1)
+	else if nb < 0 || nb >= 13 {
+		return 0
+	}
+
+	else {
+		result = nb * RecursiveFactorial(nb-1)
+	}
 
 	return result
+
 }
