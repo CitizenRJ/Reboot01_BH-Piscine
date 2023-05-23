73ce32a0c9aff47c180ac8c193961bfacba178db
rjamal <rami@piercer.gg>
2023-05-24 01:07:28 +0300

    Quest5

--- capitalize.go
+++ capitalize.go
@@ -2,13 +2,22 @@ package piscine
 
 func Capitalize(s string) string {
 	r := []rune(s)
-	for index, letter := range r {
-		if Alpha(letter) {
-			if index == 0 || Alpha(r[index-1]) == false {
-			} else if letter >= 'a' && letter <= 'z' {
-				r[index] = letter + 32
+	first := true
+	for i = 0; i < len(runes); i++ {
+		if runes[i] == ' ' {
+			first = true
+			continue
+		}
+		if first {
+			if runes[i] >= 'A' && runes[i] <= 'Z' {
+				runes[i] = runes[i] - 'A' + 'a'
 			}
+			first = false
+			continue
+		}
+		if runes[i] >= 'a' && runes[i] <= 'z' {
+			runes[i] = runes[i] - 'a' + 'A'
 		}
 	}
-	return string(r)
+	return string(runes)
 }
