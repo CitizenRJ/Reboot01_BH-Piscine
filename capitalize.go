73692db9c4a4d9fce30368ab33ca29bd3d2b79a8
rjamal <rami@piercer.gg>
2023-05-24 02:01:13 +0300

    Quest5

--- capitalize.go
+++ capitalize.go
@@ -11,14 +11,14 @@ func Capitalize(s string) string {
 	first := true
 	r := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if first && Alpha(r[i]) {
-			if r[i] >= 'a' && r[i] <= 'z' {
-				r[i] = r[i] + 'A' - 'a'
+		if first && Alpha(rune[i]) {
+			if rune[i] >= 'a' && rune[i] <= 'z' {
+				rune[i] = rune[i] + 'A' - 'a'
 			}
 			first = false
-		} else if r[i] >= 'A' && r[i] <= 'Z' {
-			r[i] = r[i] + 'a' - 'A'
-		} else if !Alpha(r[i]) {
+		} else if rune[i] >= 'A' && rune[i] <= 'Z' {
+			rune[i] = rune[i] + 'a' - 'A'
+		} else if !Alpha(rune[i]) {
 			first = true
 		}
 	}
