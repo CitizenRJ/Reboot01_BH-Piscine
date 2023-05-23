c979e4ac194a493e6ad53c74ebd41d219e132a1d
rjamal <rami@piercer.co>
2023-05-23 16:10:06 +0300

    Quest5

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,10 +1,12 @@
 package piscine
 
-import ("github.com/01-edu/z01")
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbrInOrder(n int) {
-	x:= [32]int{}
-	if n >= 0 && n<= 9 {
+	x := [32]int{}
+	if n >= 0 && n <= 9 {
 		z01.PrintRune(rune(n + '0'))
 	} else {
 		for {
@@ -16,7 +18,7 @@ func PrintNbrInOrder(n int) {
 		}
 		for index, z := range x {
 			if z != 0 {
-				for i:= 1; i <= z; i++ {
+				for i := 1; i <= z; i++ {
 					z01.PrintRune(rune(index + '0'))
 				}
 			}
