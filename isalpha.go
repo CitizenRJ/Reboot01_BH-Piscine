70a3f3b33b3fe6beb447c4a832d17fba8f604503
rjamal <rami@piercer.co>
2023-05-23 16:56:48 +0300

    Quest5

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, i := range s {
-		if !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' && i <= '9')) {
+		if i > 'z' || i < '0' {
 			return false
 		}
 	}
