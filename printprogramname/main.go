024fa703fa7013df32ab633daa2b7bbacfc4dad6
rjamal <RAMI@PIERCER.GG>
2023-05-24 17:02:24 +0300

    Quest6

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,13 +1,14 @@
 package main
 
-import( "github.com/01-edu/z01"
-		"os"
+import(
+	"github.com/01-edu/z01"
+
+	"os"
 )
 
 func main () {
 	arguments := os.Args
 	name := arguments[0]
-
 	runes := []rune(name)
 	for i := range runes {
 		z01.PrintRune(runes[i])
