c678737abe94e767a974b2b800cb09e476623f78
rjamal <rami@piercer.gg>
2023-05-24 05:49:30 +0300

    Quest5

--- trimatoi.go
+++ trimatoi.go
@@ -1,31 +1,16 @@
 package piscine
 
 func TrimAtoi(s string) int {
-	neg := false
-	str := false
-	num := 0
-	
-	if j:= s[0] {
-	if j == '-'|| j == '+' || (j >= '0' && j <= '9') {
-		num = int(j - '0')
-		str = true
+	x := 1
+	y := 0
+
+	for _, i := range s {
+		if i >= '0' && i <= '9' {
+			j := int(i - '0')
+			y = y*10 + j
+		} else if i == '-' && y == 0 {
+			x = -1
+		}
 	}
-}
-	for _, j:= range s[1:] {
-		if j >= '0' && i <= '9' {
-			num = num*10 + int(j-'0')
-			str = true
-		} else if str {
-			break
-		} else if j == '-' {
-			neg = true
-			str = true
-		} else if j == '+' {
-		str = true
-	}
-}
-	if neg {
-		num = -num
-	}
-	return num
+	return x * y
 }
