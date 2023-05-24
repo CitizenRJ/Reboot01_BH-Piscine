0832fd3033fff41c9577059d00ff0f0db5b25232
rjamal <rami@piercer.gg>
2023-05-24 05:33:33 +0300

    Quest5

--- trimatoi.go
+++ trimatoi.go
@@ -4,23 +4,20 @@ func TrimAtoi(s string) int {
 	neg := false
 	str := false
 	num := 0
-	for _, i := range s {
-		if i == '-' && !str {
+	
+	for i:= 0, i < len(s); i++ {
+		j := s[i]
+		if j == '-' && !str {
 			neg = true
 			str = true
-		} else if i == '+' && !str {
+		} else if j == '+' && !str {
 			str = true
-		} else if i >= '0' && i <= '9' {
-			num = num*10 + int(i-'0')
+		} else if j >= '0' && i <= '9' {
+			num = num*10 + int(j-'0')
 			str = true
-		} else if i != ' ' {
-			if str {
-				break
-			} else if i == 's' && len(s) > 2 && s[1] == 't' && s[2] == 'r' {
-				s = s[3:]
-			} else {
-				break
-			}
+		} else if j != ' ' {
+			return 0
+			
 		}
 	}
 	if neg {
