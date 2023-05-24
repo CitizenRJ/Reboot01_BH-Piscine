4192bc04cd66194bf02995c20b6371971ecfea1d
rjamal <rami@piercer.gg>
2023-05-24 04:57:49 +0300

    Quest5

--- trimatoi.go
+++ trimatoi.go
@@ -1,24 +1,26 @@
 package piscine
 
-negative := false
-str := false
-num := 0
-nb := rune('0')
-
-for _, c := range s {
-	if i == '-' && !str {
-		negative = true
-		str = true
+func TrimAtoi(s string) int {
+	neg := false
+	str := false
+	num := 0
+	nb := rune('0')
+	for _, c := range s {
+		if i == '-' && !str {
+			neg = true
+			str = true
 		} else if i == '+' && !str {
 			str = true
-			} else if i >= '0' && i <= '9' {
-				num = num*10 + int(i-nb)
-				str = true
-				} else if i != ' '{
-					break
-				}
-				if negative {
-					num = -num
-				}
-				return num
-			}
+		} else if i >= '0' && i <= '9' {
+			num = num*10 + int(i-nb)
+			str = true
+		} else if i != ' ' {
+			break
+		}
+		if neg {
+			num = -num
+		}
+		return num
+	}
+	return num
+}
