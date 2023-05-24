da96f882689d7dae194746c9f6c742262c7c0690
rjamal <rami@piercer.gg>
2023-05-24 22:40:35 +0300

    Quest6

--- revparams/main.go
+++ revparams/main.go
@@ -8,8 +8,11 @@ import (
 
 func main() {
 	a := os.Args[1:]
-	for i := len(a) - 1; i >= 0; i-- {
-		z01.PrintRune(rune(a[i]))
+	for i := len(a); i >= 1; i-- {
+		b:= os.Args[i]
+		for _, j := range b {
+			z01.PrintRune(rune(j))
+		}
 		z01.PrintRune('\n')
 	}
 }
