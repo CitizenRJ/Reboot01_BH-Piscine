01dcb598ebe0674d23ae92ebd8c39638a3e663b2
rjamal <RAMI@PIERCER.GG>
2023-05-24 18:29:56 +0300

    Quest6

---
+++ printparams/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main () {
+	i := os.Args[1:]
+	for _, j := range i {
+		for k := 0; k < len(j); k++ {
+			z01.PrintRune(rune(i[k]))
+		}
+		z01.PrintRune()'\n'
+	}
+}
