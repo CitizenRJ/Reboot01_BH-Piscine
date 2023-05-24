d2b80ab64eedfeb7ba16acd7c99b9fe5b1aaa4e4
rjamal <rami@piercer.gg>
2023-05-25 01:47:43 +0300

    Quest6

--- sortparams/main.go
+++ sortparams/main.go
@@ -7,10 +7,7 @@ import (
 )
 
 func main() {
-	var a int
-	for i := range os.Args {
-		a = 1
-	}
+	a := len(os.Args) - 1
 	for i := 1; i <= a; i++ {
 		for j := 1; j <= a; j++ {
 			if os.Args[i] < os.Args[j] {
@@ -18,12 +15,10 @@ func main() {
 			}
 		}
 	}
-	for i := range os.Args {
-		if i > 0 {
+		i := 1; i <= a; i++ {
 			for _, x := range os.Args[i] {
 				z01.PrintRune(x)
 			}
 			z01.PrintRune('\n')
 		}
 	}
-}
