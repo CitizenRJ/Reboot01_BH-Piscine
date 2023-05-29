941e36474264c2213fd8c02c419c7f4e265b7be3
rjamal <RAMI@PIERCER.GG>
2023-05-29 11:57:46 +0300

    Quest7

---
+++ makerange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	} else {
+		s := max - min
+		a := make([] int,s)
+		for i := 0; i < s; i++ {
+			a[i] = min + i
+		}
+		return a
+	}
+}
