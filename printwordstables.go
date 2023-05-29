a12fde896b18cb3cd22bd8f0423c18ecfc9f28df
rjamal <RAMI@PIERCER.GG>
2023-05-29 14:22:09 +0300

    Quest7

---
+++ printwordstables.go
@@ -0,0 +1,13 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(a []string) {
+	for _, i := range a {
+		for _, j := range i {
+			z01.PrintRune(s)
+		}
+		z01.PrintRune('\n')
+	}
+}
+
