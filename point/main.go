9afe7129302de95fe09e5dbc6ed32dc7855768f4
rjamal <RAMI@PIERCER.GG>
2023-05-29 15:58:46 +0300

    Quest8

---
+++ point/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+	x := int
+	y := int
+}
+
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+	
+	values := "x = 42, y = 21"
+
+	for _, i := range string {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
+}
