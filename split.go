8007505b17be39bded0cffea96d4b68720334400
rjamal <rami@piercer.gg>
2023-05-30 03:26:06 +0300

    Quest7

--- split.go
+++ split.go
@@ -1,18 +1,29 @@
 package piscine
 
 func Split(s, sep string) []string {
-	str := ""
-	line := []string{}
-	for i := 0, i < len (s)-len((sep)-1); i++ {
-		if string(s[i:i+len(sep)]) == sep {
-			line = append(line, str)
-			str = ""
-			i = i + (len(sep)-1)
+	a := []string{}
+	mot := ""
+	length := 0
+	for i, v := range s {
+		if len(s) == i+1 {
+			mot += string(v)
+			a = append(a, mot)
+		}
+		if len(sep) == length {
+			a = append(a, mot[0:len(mot)-len(sep)])
+			mot = ""
+			length = 0
+		}
+		if string(v) == string(sep[length]) {
+			length++
+			mot += string(v)
 		} else {
-			str = str + string(s[i])
+			mot += string(v)
+			length = 0
+			if string(v) == string(sep[length]) {
+				length++
+			}
 		}
 	}
-	str = str + string(s[len(s)-(len(sep)-1):])
-	line = append(line, str)
-	return line
+	return a
 }
