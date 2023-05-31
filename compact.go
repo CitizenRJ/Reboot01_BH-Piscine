978f236e75d4c8616beae6d899ba7fcf10db4076
rjamal <RAMI@PIERCER.GG>
2023-05-31 16:24:58 +0300

    Hackathon

---
+++ compact.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Compact(ptr *[]string) int {
+	count := 0
+	temp := []string
+	for i := 0; i< len(*ptr); i++ {
+		if (*prt)[i] != "" {
+			temp = append(temp, (*prt)[i])
+			count++
+		}
+	}
+	*ptr = temp
+	return count
+}
