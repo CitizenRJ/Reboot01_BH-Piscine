cbbd395f8929ccd2651f382f4dd652b49e5ec172
rjamal <RAMI@PIERCER.GG>
2023-05-31 16:42:15 +0300

    Hackathon

--- compact.go
+++ compact.go
@@ -4,8 +4,8 @@ func Compact(ptr *[]string) int {
 	count := 0
 	var temp []string
 	for i := 0; i < len(*ptr); i++ {
-		if (*prt)[i] != "" {
-			temp = append(temp, (*prt)[i])
+		if (*ptr)[i] != "" {
+			temp = append(temp, (*ptr)[i])
 			count++
 		}
 	}
