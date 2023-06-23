ca9ff58a7091742bba6419f24442d4b983acae52
rjamal <rami@piercer.gg>
2023-06-23 16:35:05 +0300

    Quest 11

--- listpushback.go
+++ listpushback.go
@@ -1,15 +1,26 @@
 package piscine
 
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
 func ListPushBack(l *List, data interface{}) {
+
 	n := &NodeL{Data: data}
+
 	if l.Head == nil {
 		l.Head = n
-		return
+	} else {
+		currentNode := l.Head
+		for currentNode.Next != nil {
+			currentNode = currentNode.Next
+		}
+		currentNode.Next = n
 	}
-
-	listItem := l.Head
-	for listItem.Next != nil {
-		listItem = listItem.Next
-	}
-	listItem.Next = n
 }
