943905e64bdce71213d9ddfdc3d819b9657f4e2c
rjamal <rami@piercer.gg>
2023-06-24 22:34:01 +0300

    Quest 12

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,20 +1,11 @@
 package piscine
 
-//type TreeNode struct {
-//	Data  string
-//	Left  *TreeNode
-//	Right *TreeNode
-//}
-
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root == nil {
-		return nil
-	}
-	if root.Data == elem {
+	if root == nil || root.Data == elem {
 		return root
-	} else if root.Data > elem {
-		return BTreeSearchItem(root.Left, elem)
-	} else {
-		return BTreeSearchItem(root.Right, elem)
 	}
+	if root.Data > elem {
+		return BTreeSearchItem(root.Left, elem)
+	}
+	return BTreeSearchItem(root.Right, elem)
 }
