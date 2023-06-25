c9218d8cee5355a5654b30154cad3aaf25d981c6
rjamal <rami@piercer.gg>
2023-06-25 19:48:04 +0300

    Quest 12

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,17 +1,21 @@
 package piscine
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	if root == nil {
-		return nil
-	}
-	new := node
-	if node.Parent == nil {
+	if node == root {
 		root = rplc
 	} else if node == node.Parent.Left {
-		new.Parent.Left = rplc
+		node.Parent.Left = rplc
 	} else {
-		new.Parent.Right = rplc
+		node.Parent.Right = rplc
 	}
-	new.Parent = node.Parent
+	if rplc != nil {
+		rplc.Parent = node.Parent
+	}
+	if node.Left != nil {
+		node.Left.Parent = rplc
+	}
+	if node.Right != nil {
+		node.Right.Parent = rplc
+	}
 	return root
 }
